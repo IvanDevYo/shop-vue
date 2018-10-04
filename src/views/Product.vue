5b37da7c6aacd6dda04d75cf0975aaaa531efcd4
ivandevyo <ivandevyo@quartsoft.com>
2018-10-04 09:45:03 +0300

    03.10 v5

---
+++ src/views/Product.vue
@@ -0,0 +1,432 @@
+<template>
+    <div class="col-lg-9 col-12">
+					<div class="breadcrumbs">	
+						<ul class="breadcrumbs-list">
+							<li class="breadcrumbs-item">
+								<a href="/" class="breadcrumbs-link">Главная</a>
+							</li>
+							<li class="breadcrumbs-item">
+								<a href="/" class="breadcrumbs-link">Каталог</a>
+							</li>
+							<li class="breadcrumbs-item"></li>
+						</ul>
+					</div>
+					<div class="product">
+						<div class="product-title">
+							Котел газовый настенный BOSCH GAZ 6000 двухконтурный с закрытой камерой сгорания 24 кВт (Ru)
+						</div>
+						<div class="row product-row">
+							<div class="custom-col-6">
+								<div class="product-slider">
+									<div class="owl-product owl-carousel">
+										<div class="product-img" data-hash="1">
+											<img src="img/cotel-1.png" alt="" class="product-pic">
+										</div>
+										<div class="product-img" data-hash="2">
+											<img src="img/cotel-2.png" alt="" class="product-pic">
+										</div>
+										<div class="product-img" data-hash="3">
+											<img src="img/cotel-3.png" alt="" class="product-pic">
+										</div>
+									</div>
+								</div>
+								<div class="product__nav-slider">
+									<div class="owl-product-nav owl-carousel">
+										<a href="#1" class="product__nav-item" style="background-image: url(img/cotel-1.png);"></a>
+										<a href="#2" class="product__nav-item" style="background-image: url(img/cotel-2.png);"></a>
+										<a href="#3" class="product__nav-item" style="background-image: url(img/cotel-3.png);"></a>
+										<a href="#3" class="product__nav-item" style="background-image: url(img/cotel-3.png);"></a>
+										<a href="#3" class="product__nav-item" style="background-image: url(img/cotel-3.png);"></a>
+									</div>
+								</div>
+							</div>
+							<div class="custom-col-6">
+								<div class="product-price">
+									37 258 руб.
+								</div>
+								<div class="product__right__info">
+									<div class="product__man__info">
+										<div class="product__man__info-item">
+											<div class="product__man__info-key">
+												КОД ТОВАРА:
+											</div>
+											<div class="product__man__info-value">
+												101915
+											</div>
+										</div>
+										<div class="product__man__info-item">
+											<div class="product__man__info-key">
+												ПРОИЗВОДИТЕЛЬ:
+											</div>
+											<div class="product__man__info-value">
+												ROCCA
+											</div>
+										</div>
+										<div class="product__man__info-item">
+											<div class="product__man__info-key">
+												СЕРИЯ:
+											</div>
+											<div class="product__man__info-value">
+												SURESTE
+											</div>
+										</div>
+										<div class="product__man__info-item">
+											<div class="product__man__info-key">
+												МОДЕЛЬ:
+											</div>
+											<div class="product__man__info-value">
+												SURESTE 170X70
+											</div>
+										</div>
+										<div class="product__man__info-item">
+											<div class="product__man__info-key">
+												СТРАНА:
+											</div>
+											<div class="product__man__info-value">
+												ИСПАНИЯ
+											</div>
+										</div>
+									</div>
+									<div class="product__adder">
+										<div class="product__adder-counter">
+											<a href="#" class="product__adder-link minus">
+												<img src="img/minus-sign.png" alt="" class="product__adder-pic">
+											</a>
+											<div class="product__adder-counter-text">
+												<span>1</span>
+											</div>
+											<a href="#" class="product__adder-link plus">
+												<img src="img/plus-sign.png" alt="" class="product__adder-pic">
+											</a>
+										</div>
+										<a href="#" class="product__adder-button">
+											<span>В корзину</span>
+										</a>
+									</div>
+									<div class="pay__method">
+										<div class="pay__method-title">
+											Способы оплаты:
+										</div>
+										<a href="#" class="pay__method-link pay-active">
+											<span>Наличные</span>
+										</a>
+										<a href="#" class="pay__method-link">
+											<span>Безнал</span>
+										</a>
+									</div>
+									<div class="characteristics">
+										<div class="characteristics-title">
+											ХАРАКТЕРИСТИКИ:
+										</div>
+										<div class="characteristics-list">
+											<div class="characteristics-list-title">
+												Габариты
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Высота, мм.
+												</div>
+												<div class="characteristics-value">
+													730 
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Ширина, мм.
+												</div>
+												<div class="characteristics-value">
+													400
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Длина, мм.
+												</div>
+												<div class="characteristics-value">
+													300.00
+												</div>
+											</div>
+										</div>
+										<div class="characteristics-list">
+											<div class="characteristics-list-title">
+												Общее
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Производитель
+												</div>
+												<div class="characteristics-value">
+													Bosch 
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Артикул
+												</div>
+												<div class="characteristics-value">
+													7736900198RU
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Серия
+												</div>
+												<div class="characteristics-value">
+													GAZ 6000
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Модель
+												</div>
+												<div class="characteristics-value">
+													Gaz 6000 W WBN 6000-24 С 
+												</div>
+											</div>
+											<div class="characteristics-item not-dotted">
+												<div class="characteristics-key">
+													Страна 
+												</div>
+												<div class="characteristics-value">
+													Россия
+												</div>
+											</div>
+										</div>
+										<div class="characteristics-list invis">
+											<div class="characteristics-list-title">
+												Габариты
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Высота, мм.
+												</div>
+												<div class="characteristics-value">
+													730 
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Ширина, мм.
+												</div>
+												<div class="characteristics-value">
+													400
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Длина, мм.
+												</div>
+												<div class="characteristics-value">
+													300.00
+												</div>
+											</div>
+										</div>
+										<div class="characteristics-list invis">
+											<div class="characteristics-list-title">
+												Габариты
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Высота, мм.
+												</div>
+												<div class="characteristics-value">
+													730 
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Ширина, мм.
+												</div>
+												<div class="characteristics-value">
+													400
+												</div>
+											</div>
+											<div class="characteristics-item">
+												<div class="characteristics-key">
+													Длина, мм.
+												</div>
+												<div class="characteristics-value">
+													300.00
+												</div>
+											</div>
+										</div>
+										<a href="#" class="product__read__more-link">
+											ПОКАЗАТЬ ВСЕ ХАРАКТЕРИСТИКИ
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="product__tabs">
+							<ul class="tabs__link-list">
+								<li class="tabs__link-item tabs__link_active">
+									<a href="#" class="tabs__link-link" id="tab1">
+										<span>Описание</span>
+									</a>
+								</li>
+								<li class="tabs__link-item">
+									<a href="#" class="tabs__link-link" id="tab2">
+										<span>Отзывы</span>
+									</a>
+								</li>
+								<li class="tabs__link-item tabs__link-item_empty"></li>
+							</ul>
+							<div class="tabs__content-list">
+								<div class="tabs__content-item tabs__content_active" id="tab__content1">
+									Габариты<br>
+									Высота, мм 	730 Ширина, мм 	400 Длина, мм 	300,00
+									<br>Общее<br>
+									Производитель 	Bosch Артикул 	7736900198RU Серия 	GAZ 6000 Модель 	Gaz 6000 W WBN 6000-24 С Страна 	Россия
+									<br>Дополнительная информация<br>
+									Тип 	газовый Тип установки 	настенный Мощность, кВт 	24 MAX температура ГВС 	+60 MAX температура отопления, °C 	+82 Погодозависимое управление 	нет
+								</div>
+								<div class="tabs__content-item" id="tab__content2">
+									Габариты
+									Высота, мм 	730 Ширина, мм 	400 Длина, мм 	300,00
+									Общее
+									Производитель 	Bosch Артикул 	7736900198RU Серия 	GAZ 6000 Модель 	Gaz 6000 W WBN 6000-24 С Страна 	Россия
+								</div>
+							</div>
+						</div>
+						<div class="product__analogs">
+							<div class="title_aftered">
+								Аналоги
+							</div>
+							<div class="row analog__products-list">
+								<div class="col-lg-4 col-md-6 product__card-container">
+									<a href="#" class="product__card">
+										<div class="product__card-image">
+											<img src="img/cotel-1.png" alt="" class="product__card-pic">
+										</div>
+										<div class="product__card-title">
+											Котел напольный BAXI SLIM EF 1.22, энергонезависимый 22 кВт, котел напольный СЛИМ
+										</div>
+										<div class="product__card-bottom">
+											<div class="product__card-art">
+												Артикуль. 00-111-2323-222
+											</div>
+											<div class="product__card-availability available">
+												<span>В наличии</span>
+											</div>
+											<div class="row">
+												<div class="col-6">
+													<div class="product__card-link_container">
+														<button class="product__card-button">
+															<img src="img/shopping-cart.png" alt="" class="product__card-icon">
+															<span>В корзину</span>
+														</button>
+													</div>
+												</div>
+												<div class="col-6 no-padding-left">
+													<div class="product__card-old-price"></div>
+													<div class="product__card-price">
+														12 100 <span>руб</span>	
+													</div>
+													<div class="product__card-price-ad">
+														за шт.
+													</div>
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								<div class="col-lg-4 col-md-6 product__card-container">
+									<a href="#" class="product__card">
+										<div class="product__card-image">
+											<img src="img/cotel-2.png" alt="" class="product__card-pic">
+										</div>
+										<div class="product__card-title">
+											Котел газовый настенный Viessmann Vitopend 100-W двухконтурный
+										</div>
+										<div class="product__card-bottom">
+											<div class="product__card-art">
+												Артикуль. 00-111-2323-222
+											</div>
+											<div class="product__card-availability available">
+												<span>В наличии</span>
+											</div>
+											<div class="row">
+												<div class="col-6">
+													<div class="product__card-link_container">
+														<button class="product__card-button">
+															<img src="img/shopping-cart.png" alt="" class="product__card-icon">
+															<span>В корзину</span>
+														</button>
+													</div>
+												</div>
+												<div class="col-6 no-padding-left">
+													<div class="product__card-old-price"></div>
+													<div class="product__card-price">
+														47 100 <span>руб</span>	
+													</div>
+													<div class="product__card-price-ad">
+														за шт.
+													</div>
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								<div class="col-lg-4 col-md-6 product__card-container">
+									<a href="#" class="product__card">
+										<div class="product__card-image">
+											<img src="img/cotel-3.png" alt="" class="product__card-pic">
+										</div>
+										<div class="product__card-title">
+											Котел газовый настенный BOSCH GAZ 4000 W битермический
+										</div>
+										<div class="product__card-bottom">
+											<div class="product__card-art">
+												Артикуль. 00-111-2323-222
+											</div>
+											<div class="product__card-availability available">
+												<span>В наличии</span>
+											</div>
+											<div class="row">
+												<div class="col-6">
+													<div class="product__card-link_container">
+														<button class="product__card-button">
+															<img src="img/shopping-cart.png" alt="" class="product__card-icon">
+															<span>В корзину</span>
+														</button>
+													</div>
+												</div>
+												<div class="col-6 no-padding-left">
+													<div class="product__card-old-price"></div>
+													<div class="product__card-price">
+														32 100 <span>руб</span>	
+													</div>
+													<div class="product__card-price-ad">
+														за шт.
+													</div>
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'Product',
+    computed: {
+        ...mapGetters('products', ['currentProduct']),
+        cProduct() {
+			const productUrl = this.$route.params.product;
+			return this.currentProduct(productUrl);
+		}
+    }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
